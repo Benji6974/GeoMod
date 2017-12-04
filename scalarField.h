1c58562338a1d6592a926fed4f521db3979569f1
CAMPODIFIORI FLORIAN p1409693 <p1409693@b710l0809.univ-lyon1.fr>
2017-12-04 17:02:20 +0100

    ajout load + modif scalar

--- scalarField.h
+++ scalarField.h
@@ -1,17 +1,21 @@
 #ifndef SCALARFIELD_H
 #define SCALARFIELD_H
 #include "array2.h"
-#include <string>
+#include <QString>
+#include <QPixmap>
 
 class ScalarField: public Array2
 {
 public:
     ScalarField();
+    ScalarField(int nx, int ny, vec2 a, vec2 b);
     int index(int i,int j);
     vec3 P(int i,int j);
     bool inside(int x,int y,int z);
     double height(const double& x, const double& y);
     double heightGrid(int i,int j);
+
+    void load(QString img,vec2 a, vec2 b,float za, float zb);
 
 protected:
     double z[]; //Array of height
